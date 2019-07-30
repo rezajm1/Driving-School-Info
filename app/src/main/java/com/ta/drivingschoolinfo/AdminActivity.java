c6847042b2037e6357c66b955be31ac38c81b9a2
rezza jaya mukti <rezajm19@gmail.com>
2019-07-30 13:49:00 +0700

    login udah lancar bisa logout,kalo login,login seterusnya,kalo keluar tinggal log out

--- app/src/main/java/com/ta/drivingschoolinfo/AdminActivity.java
+++ app/src/main/java/com/ta/drivingschoolinfo/AdminActivity.java
@@ -18,80 +18,25 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class AdminActivity extends AppCompatActivity {
 
-        private Button signOut;
-
-
-    private ProgressBar progressBar;
-    private FirebaseAuth.AuthStateListener authListener;
-    private FirebaseAuth auth;
+    Button btnLogOut;
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
-
-        //get firebase auth instance
-        auth = FirebaseAuth.getInstance();
-
-        //get current user
-        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-        authListener = new FirebaseAuth.AuthStateListener() {
+        btnLogOut = (Button) findViewById(R.id.sign_out);
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user == null) {
-                    // user auth state is changed - user is null
-                    // launch login activity
-                    startActivity(new Intent(AdminActivity.this, HomeActivity.class));
-                    finish();
-                }
-            }
-        };
+            public void onClick(View view) {
 
+                FirebaseAuth.getInstance().signOut();
+                Intent I = new Intent(AdminActivity.this, LoginActivity.class);
+                startActivity(I);
 
-        signOut = (Button) findViewById(R.id.sign_out);
-
-
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-
-        if (progressBar != null) {
-            progressBar.setVisibility(View.GONE);
-        }
-
-
-
-        signOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signOut();
             }
         });
 
-    }
-
-    //sign out method
-    public void signOut() {
-        auth.signOut();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        progressBar.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        auth.addAuthStateListener(authListener);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (authListener != null) {
-            auth.removeAuthStateListener(authListener);
-        }
     }
 }
--- app/src/main/java/com/ta/drivingschoolinfo/LoginActivity.java
+++ app/src/main/java/com/ta/drivingschoolinfo/LoginActivity.java
@@ -1,10 +1,18 @@
 package com.ta.drivingschoolinfo;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+//import android.view.WindowManager;
+import android.content.Intent;
+
+import android.support.annotation.NonNull;
+
+import android.text.TextUtils;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -16,75 +24,88 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText userMail,userPassword;
-    private Button btnLogin;
-    private FirebaseAuth mAuth;
-    private Intent HomeActivity;
-    private ProgressBar loginprogress;
+
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
+    private Button btnLogin ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        userMail = findViewById(R.id.email);
-        userPassword = findViewById(R.id.password);
-        btnLogin = findViewById(R.id.btn_login);
-        loginprogress = findViewById(R.id.progressBar);
-        mAuth = FirebaseAuth.getInstance();
-        HomeActivity = new Intent(this,AdminActivity.class);
 
-        loginprogress.setVisibility(View.INVISIBLE);
+//START KODE UTAMA
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, AdminActivity.class));
+            finish();
+        }
+
+
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginprogress.setVisibility(View.INVISIBLE);
-                btnLogin.setVisibility(View.VISIBLE);
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
 
-                final String mail = userMail.getText().toString();
-                final String password = userPassword.getText().toString();
-
-                if(mail.isEmpty()||password.isEmpty()){
-                    showMessage("Lengkapi Semua Kolom");
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Masukkan Email", Toast.LENGTH_SHORT).show();
+                    return;
                 }
-                else {
 
-                    signin(mail,password);
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Masukkan password!", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        inputPassword.setError(getString(R.string.minimum_password));
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
             }
         });
 
-    }
+//END KODE UTAMA
 
-    private void signin(String mail, String password) {
-
-        mAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-
-
-                if (task.isSuccessful()){
-
-                    loginprogress.setVisibility(View.INVISIBLE);
-                    btnLogin.setVisibility(View.VISIBLE);
-                    updateUI();
-                }
-                else
-                    showMessage(task.getException().getMessage());
-
-            }
-        });
+//FLOATING BAR DEFAULT
 
     }
 
-    private void updateUI() {
-
-        startActivity(HomeActivity);
-        finish();
-
-    }
-
-    private void showMessage(String text) {
-        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_LONG).show();
-    }
 }
